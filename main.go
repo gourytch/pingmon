84e5ea22d12847289015f5f721171ef84a932df1
Lev Babchenko <gour@mail.ru>
2023-12-14 09:59:21 +0100

    merge Monitor and Watcher

--- main.go
+++ main.go
@@ -10,130 +10,16 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"strings"
-	"sync"
 	"syscall"
-	"time"
 
 	"github.com/joho/godotenv"
 )
 
 const DEFAULT_HOSTLIST = "1.1.1.1 8.8.8.8"
-
-var NullTime time.Time = time.Time{}
-
-type Processor struct {
-	storage Storage
-	evlock  sync.Mutex
-	events  map[string]Event
-}
-
-func NewProcessor(storage Storage) *Processor {
-	return &Processor{
-		storage: storage,
-		evlock:  sync.Mutex{},
-		events:  map[string]Event{},
-	}
-}
-
-func (p *Processor) Process(sample Sample) {
-	if !sample.At.After(NullTime) || sample.Address == "" {
-		panic(fmt.Sprintf("bad sample %#v", sample))
-	}
-	p.evlock.Lock()
-	defer p.evlock.Unlock()
-	evt, ok := p.events[sample.Address]
-	// log.Printf("sample %+v", sample)
-	// log.Printf("event  %+v", evt)
-
-	if !ok {
-		// an active event is not found for this address. start the new one
-		evt = Event{
-			Address:  sample.Address,
-			At:       sample.At,
-			Duration: 0,
-			Online:   sample.IsOnline(),
-		}
-		p.events[sample.Address] = evt
-		if err := p.storage.EventOpen(evt); err != nil {
-			log.Printf("EventOpen(%v) error: %s", evt, err.Error())
-		}
-		log.Printf("%s is %s", sample.Address, b2s[sample.IsOnline()])
-		return
-	} else {
-		// an active event for this address is exist
-		dt := sample.At.Sub(evt.At)
-		if dt < evt.Duration {
-			// an obsolete event. just ignore it
-			// log.Printf("AN OBSOLETE SAMPLE: %s", sample)
-			return
-		} else {
-			evt.Duration = dt // update duration for the event
-			if sample.IsOnline() == evt.Online {
-				// A continue indicator for the active event. Update it but don't save it
-				// log.Printf("AN ENLARGER: %s", sample)
-				p.events[sample.Address] = evt
-				if err := p.storage.EventUpdate(evt); err != nil {
-					log.Printf("EventUpdate(%v) error: %s", evt, err.Error())
-				}
-				return
-			} else {
-				// switch to the new state
-				// log.Printf("A SWITCHER: %s", sample)
-				if err := p.storage.EventClose(evt); err != nil {
-					log.Printf("EventClose(%v) error: %s", evt, err.Error())
-				}
-				// start the new event
-				p.events[sample.Address] = Event{
-					Address:  sample.Address,
-					At:       sample.At,
-					Duration: 0,
-					Online:   sample.IsOnline(),
-				}
-				log.Printf("%s switched to %s", sample.Address, b2s[sample.IsOnline()])
-				return
-			}
-			// panic("unreachable point")
-		}
-		// panic("unreachable point")
-	}
-	// panic("unreachable point")
-}
-
-func watch(ctx context.Context, samples <-chan Sample) {
-
-	stg, err := NewSqliteStorage("pingmon.sqlite")
-	if err != nil {
-		panic(err)
-	}
-	defer stg.Close()
-	processor := NewProcessor(stg)
-
-	log.Printf("watcher started")
-	for {
-		select {
-		case <-ctx.Done():
-			log.Printf("watcher finished")
-			return
-		case sample := <-samples:
-			// log.Println(sample.String())
-			if err := stg.Add(sample); err != nil {
-				log.Printf("Add(%v) error: %s", sample, err.Error())
-			}
-			processor.Process(sample)
-		}
-	}
-}
-
-func test(ctx context.Context, samples chan<- Sample, addr string) {
-	go func() {
-		MonitorForever(ctx, addr, samples)
-	}()
-}
 
 func main() {
 	hosts := []string{}
@@ -162,16 +48,15 @@ func main() {
 	log.Printf("the next hosts will be monitored: %#v", hosts)
 
 	ctx, cancel := context.WithCancel(context.Background())
-	samples := make(chan Sample, 100)
-	go watch(ctx, samples)
+	watcher := NewWatcher(ctx, "pingmon.sqlite")
 	for _, host := range hosts {
-		test(ctx, samples, host)
+		watcher.AddHost(host)
 	}
 	osch := make(chan os.Signal, 1)
 	signal.Notify(osch, syscall.SIGINT, syscall.SIGTERM)
 	<-osch
 	log.Println("shutting down...")
 	cancel()
-	time.Sleep(1 * time.Second)
+	watcher.wg.Wait()
 	log.Println("done.")
 }
